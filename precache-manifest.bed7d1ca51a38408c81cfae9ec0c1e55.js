1e280976136493f05650182442c85d08b0126ed0
dilkash133 <dilkash133@gmail.com>
2020-07-25 07:50:35 +0530

    Updates

---
+++ precache-manifest.bed7d1ca51a38408c81cfae9ec0c1e55.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc61da4f4a241543849cdb1fd2d2974d",
+    "url": "/ghost/index.html"
+  },
+  {
+    "revision": "5edd81bcf8ed33d3ee42",
+    "url": "/ghost/static/css/main.33fe8d56.chunk.css"
+  },
+  {
+    "revision": "c324d6e88f8dfd411942",
+    "url": "/ghost/static/js/2.6d64adf5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ghost/static/js/2.6d64adf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5edd81bcf8ed33d3ee42",
+    "url": "/ghost/static/js/main.3247b4d3.chunk.js"
+  },
+  {
+    "revision": "c81f94fe345aa251c7f3",
+    "url": "/ghost/static/js/runtime-main.5f06e3e2.js"
+  },
+  {
+    "revision": "819c65e9c36fcb2dc239353a4716802f",
+    "url": "/ghost/static/media/platform.819c65e9.png"
+  },
+  {
+    "revision": "7ffacee7d00315b5f938ec9892fe2cf1",
+    "url": "/ghost/static/media/social1.7ffacee7.svg"
+  },
+  {
+    "revision": "8fedd2db85bbf119476eaeac8eb3abc3",
+    "url": "/ghost/static/media/social2.8fedd2db.svg"
+  },
+  {
+    "revision": "10e15ad855ef57fc3f73156fbaa16a8b",
+    "url": "/ghost/static/media/social3.10e15ad8.svg"
+  },
+  {
+    "revision": "7693ebb28edf5533a1ec6095cca45eb5",
+    "url": "/ghost/static/media/social4.7693ebb2.svg"
+  }
+]);
